b4e364b26e312d859c2553f5b2446ae1587d054d
Gwangmin Song <thdalsrhkd@naver.com>
2025-10-18 15:58:39 +0900

    셰이더 생성 - 16번부터는 행렬 넣어야함

--- cgtest/16.cpp
+++ cgtest/16.cpp
@@ -332,7 +332,7 @@ void make_vertexShaders()
 	GLchar* vertexSource;
 	//--- 버텍스 세이더 읽어 저장하고 컴파일 하기
 	//--- filetobuf: 사용자정의 함수로 텍스트를 읽어서 문자열에 저장하는 함수
-	vertexSource = filetobuf("vertex.glsl");
+	vertexSource = filetobuf("vertex_matrix.glsl");
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
 	glCompileShader(vertexShader);
@@ -351,7 +351,7 @@ void make_fragmentShaders()
 {
 	GLchar* fragmentSource;
 	//--- 프래그먼트 세이더 읽어 저장하고 컴파일하기
-	fragmentSource = filetobuf("fragment.glsl"); // 프래그세이더 읽어오기
+	fragmentSource = filetobuf("fragment_matrix.glsl"); // 프래그세이더 읽어오기
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
 	glCompileShader(fragmentShader);

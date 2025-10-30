884771f4bc12e7f7687530303584c401ac3aafb8
Gwangmin Song <thdalsrhkd@naver.com>
2025-10-30 12:53:59 +0900

    원 벡터에 등록

--- cgtest/19.cpp
+++ cgtest/19.cpp
@@ -380,74 +380,26 @@ int main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 	glEnable(GL_DEPTH_TEST);
 	allVertices.clear();
 
-	transformArray[0].xRotation = 0.0f;
-	transformArray[0].yRotation = 0.0f;
-	transformArray[0].localScale = 1.0f;
-	transformArray[0].position = glm::vec3(-0.2f, 0.0f, 0.0f);
-	transformArray[0].shapeType = SHAPE_CUBE; // 초기: 육면체
+	
 
-	transformArray[1].xRotation = 0.0f;
-	transformArray[1].yRotation = 0.0f;
-	transformArray[1].localScale = 1.0f;
-	transformArray[1].position = glm::vec3(0.2f, 0.0f, 0.0f);
-	transformArray[1].shapeType = SHAPE_CONE; // 초기: 12각뿔
-
-
-	// glm::vec4로 축 좌표 정의
-	glm::vec4 xaxis1(10, 0, 0, 1);
-	glm::vec4 xaxis2(-10, 0, 0, 1);
-	glm::vec4 yaxis1(0, 10, 0, 1);
-	glm::vec4 yaxis2(0, -10, 0, 1);
-	glm::vec4 zaxis1(0, 0, 10, 1);
-	glm::vec4 zaxis2(0, 0, -10, 1);
-
-	// y축으로 -30도 회전 (glm::rotate 사용)
-	/*xaxis1 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(0.0f, 1.0f, 0.0f)) * xaxis1;
-	xaxis2 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(0.0f, 1.0f, 0.0f)) * xaxis2;
-	yaxis1 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(0.0f, 1.0f, 0.0f)) * yaxis1;
-	yaxis2 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(0.0f, 1.0f, 0.0f)) * yaxis2;
-	zaxis1 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(0.0f, 1.0f, 0.0f)) * zaxis1;
-	zaxis2 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(0.0f, 1.0f, 0.0f)) * zaxis2;
-
-	// x축으로 30도 회전 (glm::rotate 사용)
-	xaxis1 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(1.0f, 0.0f, 0.0f)) * xaxis1;
-	xaxis2 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(1.0f, 0.0f, 0.0f)) * xaxis2;
-	yaxis1 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(1.0f, 0.0f, 0.0f)) * yaxis1;
-	yaxis2 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(1.0f, 0.0f, 0.0f)) * yaxis2;
-	zaxis1 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(1.0f, 0.0f, 0.0f)) * zaxis1;
-	zaxis2 = glm::rotate(glm::mat4(1.0f), (float)(-pi / 6), glm::vec3(1.0f, 0.0f, 0.0f)) * zaxis2;*/
-
-
-
-	// 축 데이터를 allVertices에 추가 (vec4의 x, y, z 멤버 사용)
-	allVertices.insert(allVertices.end(), {
-					xaxis1.x, xaxis1.y, xaxis1.z,
-					1, 0, 0 });
-	allVertices.insert(allVertices.end(), {
-					xaxis2.x, xaxis2.y, xaxis2.z,
-					1, 0, 0 });
-
-	allVertices.insert(allVertices.end(), {
-					yaxis1.x, yaxis1.y, yaxis1.z,
-					0, 1, 0 });
-	allVertices.insert(allVertices.end(), {
-					yaxis2.x, yaxis2.y, yaxis2.z,
-					0, 1, 0 });
-
-	allVertices.insert(allVertices.end(), {
-					zaxis1.x, zaxis1.y, zaxis1.z,
-					0, 0, 1 });
-	allVertices.insert(allVertices.end(), {
-					zaxis2.x, zaxis2.y, zaxis2.z,
-					0, 0, 1 });
-
-
-	/*for (auto& poly : polygonmap) {
-		poly.rotate(-pi / 6, 'y');
-		poly.rotate(-pi / 6, 'x');
-
-		poly.sendvertexdata(allVertices);
-	}*/
+	// 원을 그리기 위한 점들 생성 (0, 0, -50)을 y축으로 회전
+	int circleSegments = 100; // 원을 구성할 선분 개수
+	float radius = 50.0f; // 반지름
+	glm::vec3 startPoint(0.0f, 0.0f, -radius); // 시작점
+	
+	for (int i = 0; i <= circleSegments; ++i) {
+		float angle = (2.0f * pi * i) / circleSegments; // 0 ~ 2π
+		
+		// y축 회전 행렬 적용
+		glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 1.0f, 0.0f));
+		glm::vec4 rotatedPoint = rotationMatrix * glm::vec4(startPoint, 1.0f);
+		
+		// 점 추가
+		allVertices.insert(allVertices.end(), {
+			rotatedPoint.x, rotatedPoint.y, rotatedPoint.z,
+			1.0f, 1.0f, 0.0f // 노란색
+		});
+	}
 
 
 
@@ -575,114 +527,9 @@ GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 	glm::mat4 identityMatrix = glm::mat4(1.0);
 	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(identityMatrix)); // 버텍스 셰이더에 있는 modelTransform에 단위 행렬 전달
 	glDrawArrays(GL_LINES, 0, 6);
-
-	// GLU 객체 그리기 (고정 파이프라인 사용)
-	glUseProgram(0); // 셰이더 비활성화
-
-	// 고정 파이프라인 행렬 설정
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	// view 행렬 적용 (카메라 설정)
-	gluLookAt(cameraPos.x, cameraPos.y, cameraPos.z,
-		cameraTarget.x, cameraTarget.y, cameraTarget.z,
-		cameraUp.x, cameraUp.y, cameraUp.z);
-
-	// 첫 번째 객체 (transformArray[0])
-	glPushMatrix(); // 현재 행렬 저장
-
-	// 1. 이동 (translate)
-	glTranslatef(transformArray[0].position.x, transformArray[0].position.y, transformArray[0].position.z);
-
-	// 2. y축 자전 (yRotation)
-	glRotatef(glm::degrees(transformArray[0].yRotation), 0.0f, 1.0f, 0.0f);
-
-	// 3. x축 자전 (xRotation)
-	glRotatef(glm::degrees(transformArray[0].xRotation), 1.0f, 0.0f, 0.0f);
-
-	// 4. 제자리 scale (localScale)
-	glScalef(transformArray[0].localScale, transformArray[0].localScale, transformArray[0].localScale);
-
-	glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-
-	GLUquadricObj* qobj;
-	qobj = gluNewQuadric();
-	gluQuadricDrawStyle(qobj, GLU_FILL);
-
-	glColor3f(0.0f, 0.0f, 0.0f); // 검은색으로 설정
-
-	// shapeType에 따라 도형 그리기
-	switch (transformArray[0].shapeType) {
-	case SHAPE_CUBE: // 육면체
-		gluCylinder(qobj, 0.1, 0.1, 0.2, 4, 1);
-		break;
-	case SHAPE_SPHERE: // 구
-		gluSphere(qobj, 0.1, 20, 20);
-		break;
-	case SHAPE_CONE: // 12각뿔
-		glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-		gluCylinder(qobj, 0.1, 0.0, 0.2, 12, 1);
-		break;
-	case SHAPE_CYLINDER: // 12각기둥
-		glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-		gluCylinder(qobj, 0.1, 0.1, 0.2, 12, 1);
-		break;
-	}
-
-	gluDeleteQuadric(qobj);
-
-	glPopMatrix(); // 이전 행렬 복원
-
-	// 두 번째 객체 (transformArray[1]) - 구 그리기
-	glPushMatrix(); // 현재 행렬 저장
-
-	// 1. 이동 (translate)
-	glTranslatef(transformArray[1].position.x, transformArray[1].position.y, transformArray[1].position.z);
-
-	// 2. y축 자전 (yRotation)
-	glRotatef(glm::degrees(transformArray[1].yRotation), 0.0f, 1.0f, 0.0f);
-
-	// 3. x축 자전 (xRotation)
-	glRotatef(glm::degrees(transformArray[1].xRotation), 1.0f, 0.0f, 0.0f);
-
-	// 4. 제자리 scale (localScale)
-	glScalef(transformArray[1].localScale, transformArray[1].localScale, transformArray[1].localScale);
-
-	glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-
-	GLUquadricObj* qobj2;
-	qobj2 = gluNewQuadric();
-	gluQuadricDrawStyle(qobj2, GLU_FILL);
-
-	glColor3f(0.5f, 0.0f, 0.5f); // 보라색으로 설정
-
-	// shapeType에 따라 도형 그리기
-	switch (transformArray[1].shapeType) {
-	case SHAPE_CUBE: // 육면체
-		gluCylinder(qobj2, 0.1, 0.1, 0.2, 4, 1);
-		break;
-	case SHAPE_SPHERE: // 구
-		gluSphere(qobj2, 0.1, 20, 20);
-		break;
-	case SHAPE_CONE: // 12각뿔
-		glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-		gluCylinder(qobj2, 0.1, 0.0, 0.2, 12, 1);
-		break;
-	case SHAPE_CYLINDER: // 12각기둥
-		glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-		gluCylinder(qobj2, 0.1, 0.1, 0.2, 12, 1);
-		break;
-	}
-
-	gluDeleteQuadric(qobj2);
-
-	glPopMatrix(); // 이전 행렬 복원
-
-	// 다시 셰이더 활성화
-	glUseProgram(shaderProgramID);
+	//// 원 그리기 (LINE_STRIP 사용)
+	//glDrawArrays(GL_LINE_STRIP, 6, 100 + 1); // 6은 축 데이터 개수
+	
 
 	glutSwapBuffers(); // 화면에 출력하기
 }
@@ -701,232 +548,6 @@ void Keyboard(unsigned char key, int x, int y) {
 	case 'Q': // 프로그램 종료
 		glutLeaveMainLoop();
 		break;
-	case '1':
-		// 첫 번째 객체만 선택 (transformArray[0])
-		currentObject = 0;
-		std::cout << "Selected object: 0 only" << std::endl;
-		break;
-	case '2':
-		// 두 번째 객체만 선택 (transformArray[1])
-		currentObject = 1;
-		std::cout << "Selected object: 1 only" << std::endl;
-		break;
-	case '3':
-		// 두 객체 모두 선택 (currentObject = 2는 둘 다를 의미)
-		currentObject = 2;
-		std::cout << "Selected both objects" << std::endl;
-		break;
-	case '0':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
-	{
-		int idx = key - '0';
-		for (int i = 0; i < 10; ++i) {
-			selection[i] = 0;
-		}
-		idx += 9;
-		idx %= 10;
-		selection[idx] = 1;
-	}
-	break;
-	case 'x': // x축 양방향 자전
-		// currentObject가 2이면 두 객체 모두 적용
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].xRotation += glm::radians(5.0f);
-			}
-		}
-		break;
-	case 'X': // x축 음방향 자전
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].xRotation -= glm::radians(5.0f);
-			}
-		}
-		break;
-	case 'y': // y축 양방향 자전
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].yRotation += glm::radians(5.0f);
-			}
-		}
-		break;
-	case 'Y': // y축 음방향 자전
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].yRotation -= glm::radians(5.0f);
-			}
-		}
-		break;
-	case 'r': // y축 공전 (좌표를 y축 방향으로 회전)
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				// 현재 좌표를 glm::vec3로 변환
-				glm::vec3 currentPos(transformArray[i].position.x, transformArray[i].position.y, transformArray[i].position.z);
-				// y축으로 5도 회전하는 회전 행렬 생성
-				glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(5.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-				// 좌표에 회전 행렬 적용
-				glm::vec4 rotatedPos = rotationMatrix * glm::vec4(currentPos, 1.0f);
-				// 회전된 좌표를 다시 저장
-				transformArray[i].position = glm::vec3(rotatedPos);
-			}
-		}
-		break;
-	case 'R': // y축 역공전 (좌표를 y축 방향으로 역회전)
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				// 현재 좌표를 glm::vec3로 변환
-				glm::vec3 currentPos(transformArray[i].position.x, transformArray[i].position.y, transformArray[i].position.z);
-				// y축으로 -5도 회전하는 회전 행렬 생성
-				glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(-5.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-				// 좌표에 회전 행렬 적용
-				glm::vec4 rotatedPos = rotationMatrix * glm::vec4(currentPos, 1.0f);
-				// 회전된 좌표를 다시 저장
-				transformArray[i].position = glm::vec3(rotatedPos);
-			}
-		}
-		break;
-	case 'a': // 제자리에서 확대
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].localScale += 0.1f;
-				if (transformArray[i].localScale > 3.0f)
-					transformArray[i].localScale = 3.0f;
-			}
-		}
-		break;
-	case 'A': // 제자리에서 축소
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].localScale -= 0.1f;
-				if (transformArray[i].localScale < 0.1f)
-					transformArray[i].localScale = 0.1f;
-			}
-		}
-		break;
-	case 'b': // 원점 기준 확대 (localScale과 좌표를 함께 조정)
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				float scaleFactor = 1.1f; // 10% 확대
-				transformArray[i].localScale *= scaleFactor;
-				if (transformArray[i].localScale > 3.0f)
-					transformArray[i].localScale = 3.0f;
-				// 원점 기준이므로 위치도 확대
-				transformArray[i].position.x *= scaleFactor;
-				transformArray[i].position.y *= scaleFactor;
-				transformArray[i].position.z *= scaleFactor;
-			}
-		}
-		break;
-	case 'B': // 원점 기준 축소 (localScale과 좌표를 함께 조정)
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				float scaleFactor = 0.9f; // 10% 축소
-				transformArray[i].localScale *= scaleFactor;
-				if (transformArray[i].localScale < 0.1f)
-					transformArray[i].localScale = 0.1f;
-				// 원점 기준이므로 위치도 축소
-				transformArray[i].position.x *= scaleFactor;
-				transformArray[i].position.y *= scaleFactor;
-				transformArray[i].position.z *= scaleFactor;
-			}
-		}
-		break;
-	case 'd': // x축 좌로 이동
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].position.x -= 0.05f;
-			}
-		}
-		break;
-	case 'D': // x축 우로 이동
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].position.x += 0.05f;
-			}
-		}
-		break;
-	case 'e': // y축 위로 이동
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].position.y += 0.05f;
-			}
-		}
-		break;
-	case 'E': // y축 아래로 이동
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].position.y -= 0.05f;
-			}
-		}
-		break;
-	case 'u':
-	{
-		if (swapAnimationActive == 0) {
-			// 애니메이션 시작
-			swapAnimationActive = 1;
-			swapAnimationProgress = 0.0f;
-
-			transformArray[0].midPoint = glm::vec3(0.0f, 0.8f, 0.0f);
-			transformArray[1].midPoint = glm::vec3(0.0f, -0.8f, 0.0f);
-
-			// 시작 위치 저장
-			swapStartPos[0] = transformArray[0].position;
-			swapStartPos[1] = transformArray[1].position;
-
-			// 목표 위치 설정 (서로 교환)
-			swapEndPos[0] = transformArray[1].position;
-			swapEndPos[1] = transformArray[0].position;
-
-			std::cout << "Position swap animation started" << std::endl;
-		}
-
-	}
-	break;
-	case 't': // 위치 교환 애니메이션 시작
-	{
-		if (swapAnimationActive == 0) {
-			// 애니메이션 시작
-			swapAnimationActive = 1;
-			swapAnimationProgress = 0.0f;
-
-			transformArray[0].midPoint = glm::vec3(0.0f, 0.0f, 0.0f);
-			transformArray[1].midPoint = glm::vec3(0.0f, 0.0f, 0.0f);
-
-			// 시작 위치 저장
-			swapStartPos[0] = transformArray[0].position;
-			swapStartPos[1] = transformArray[1].position;
-
-			// 목표 위치 설정 (서로 교환)
-			swapEndPos[0] = transformArray[1].position;
-			swapEndPos[1] = transformArray[0].position;
-
-			std::cout << "Position swap animation started" << std::endl;
-		}
-	}
-	break;
-	case 'f': // opentoggle
-	{
-		opentoggle = !opentoggle;
-	}
-	break;
-	case 's': // 초기화
-	{
-		// 선택된 객체(들) 초기화
-		for (int i = 0; i < 2; ++i) {
-			if (currentObject == 2 || currentObject == i) {
-				transformArray[i].xRotation = 0.0f;
-				transformArray[i].yRotation = 0.0f;
-				transformArray[i].localScale = 1.0f;
-				transformArray[i].position = (i == 0) ? glm::vec3(-0.2f, 0.0f, 0.0f) : glm::vec3(0.2f, 0.0f, 0.0f);
-			}
-		}
-	}
-	break;
 	case 'h': // 은면제거 적용/해제
 	{
 		if (hidetoggle) {
@@ -941,23 +562,11 @@ void Keyboard(unsigned char key, int x, int y) {
 	break;
 	case 'c': // shapeType 랜덤 변경
 	{
-		// 랜덤 생성기 (0~3: SHAPE_CUBE, SHAPE_SPHERE, SHAPE_CONE, SHAPE_CYLINDER)
-		std::uniform_int_distribution<int> shapeDis(0, 3);
-
-		// 선택된 객체(들)의 shapeType을 랜덤으로 변경
-		transformArray[0].shapeType = static_cast<ShapeType>(shapeDis(gen));
-		transformArray[1].shapeType = static_cast<ShapeType>(shapeDis(gen));
 	}
 	break;
 	case 'v': // y축 회전 애니메이션 토글
 	{
-		yRotationAnimationActive = !yRotationAnimationActive;
-		if (yRotationAnimationActive) {
-			std::cout << "Y-axis rotation animation started" << std::endl;
-		}
-		else {
-			std::cout << "Y-axis rotation animation stopped" << std::endl;
-		}
+		
 	}
 	break;
 	case '+':
@@ -982,58 +591,7 @@ void Keyboard(unsigned char key, int x, int y) {
 
 void TimerFunction(int value)
 {
-	// 위치 교환 애니메이션 처리
-	if (swapAnimationActive == 1) {
-		swapAnimationProgress += 0.02f; // 애니메이션 속도 (0.02씩 증가)
-
-		if (swapAnimationProgress >= 1.0f) {
-			// 애니메이션 완료
-			swapAnimationProgress = 1.0f;
-			swapAnimationActive = 0;
-
-			// 최종 위치로 설정
-			transformArray[0].position = swapEndPos[0];
-			transformArray[1].position = swapEndPos[1];
-
-			std::cout << "Position swap animation completed" << std::endl;
-		}
-		else {
-			// 선형보간 (glm::mix 사용)
-			// 0.0 ~ 0.5: 시작 위치 -> 원점
-			// 0.5 ~ 1.0: 원점 -> 목표 위치
-
-			if (swapAnimationProgress <= 0.5f) {
-				// 첫 번째 단계: 시작 위치 -> 원점
-				float t = swapAnimationProgress * 2.0f; // 0.0 ~ 1.0으로 정규화
-				transformArray[0].position = glm::mix(swapStartPos[0], transformArray[0].midPoint, t);
-				transformArray[1].position = glm::mix(swapStartPos[1], transformArray[1].midPoint, t);
-			}
-			else {
-				// 두 번째 단계: 원점 -> 목표 위치
-				float t = (swapAnimationProgress - 0.5f) * 2.0f; // 0.0 ~ 1.0으로 정규화
-				transformArray[0].position = glm::mix(transformArray[0].midPoint, swapEndPos[0], t);
-				transformArray[1].position = glm::mix(transformArray[1].midPoint, swapEndPos[1], t);
-			}
-		}
-	}
-
-	// y축 회전 애니메이션 처리
-	if (yRotationAnimationActive) {
-		for (int i = 0; i < 2; ++i) {
-			transformArray[i].yRotation += glm::radians(2.0f); // 매 타이머마다 2도 증가
-			glm::vec3 currentPos(transformArray[i].position.x, transformArray[i].position.y, transformArray[i].position.z);
-			// y축으로 -5도 회전하는 회전 행렬 생성
-			glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(-5.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-			// 좌표에 회전 행렬 적용
-			glm::vec4 rotatedPos = rotationMatrix * glm::vec4(currentPos, 1.0f);
-			// 회전된 좌표를 다시 저장
-			transformArray[i].position = glm::vec3(rotatedPos);
-		}
-		transformArray[0].localScale += 0.01f;
-		if (transformArray[1].localScale < 0.03f)
-			transformArray[1].localScale -= 0.01f;
-
-	}
+	
 
 	glutPostRedisplay();
 	glutTimerFunc(25, TimerFunction, 1);

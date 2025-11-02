3e5c303fa4e819028b58c13c5afb50a5a35e22ee
Gwangmin Song <thdalsrhkd@naver.com>
2025-11-02 11:00:34 +0900

    초기세팅 오류 제거

--- cgtest/20.cpp
+++ cgtest/20.cpp
@@ -232,20 +232,12 @@ GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// 카메라 설정
-	glm::vec3 cameraPos = glm::vec3(0.0f, 10.0f, 150.0f) + additionalCameraPos;
+	glm::vec3 cameraPos = glm::vec3(0.0f, 10.0f, 150.0f);
 	glm::vec3 cameraTarget = glm::vec3(cameraPos.x, cameraPos.y - 10.0f, cameraPos.z - 150.0f);
 	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 	
 
-	if (projectiontoggle == 0) {
-		// 원근 투영
-		gluPerspective(60.0, 1.0, 0.1, 300.0);
-	}
-	else {
-		// 직교 투영
-		glOrtho(-100.0, 100.0, -100.0, 100.0, 0.1, 300.0);
-	}
 
 	
 
@@ -273,48 +265,12 @@ void Keyboard(unsigned char key, int x, int y) {
 	break;
 	case 'z': // z축 시계방향 회전
 	{
-		if (zrotationdirection == 1) {
-			zrotationdirection = 0; // 이미 1이면 0으로
-		}
-		else {
-			zrotationdirection = 1; // 아니면 1로
-		}
+		
 	}
 	break;
 	case 'Z': // z축 반시계방향 회전
 	{
-		if (zrotationdirection == -1) {
-			zrotationdirection = 0; // 이미 -1이면 0으로
-		}
-		else {
-			zrotationdirection = -1; // 아니면 -1로
-		}
-	}
-	break;
-	case 'y': // 궤도 스케일 증가
-	{
-		orbitscale += 0.05f;
-	}
-	break;
-	case 'Y': // 궤도 스케일 감소
-	{
-		orbitscale -= 0.05f;
-		if (orbitscale < 0.1f) orbitscale = 0.1f; // 최소값 제한
-	}
-	break;
-	case 'p': // 투영 모드 변경
-	{
-		projectiontoggle = !projectiontoggle;
-	}
-	break;
-	case '+': // z축 앞으로 이동
-	{
-		additionalCameraPos.z -= 5.0f;
-	}
-	break;
-	case '-': // z축 뒤로 이동
-	{
-		additionalCameraPos.z += 5.0f;
+		
 	}
 	break;
 	case 'h': // 은면제거 적용/해제
@@ -365,144 +321,14 @@ void SpecialKeys(int key, int x, int y) // 특수 키(화살표 키) 콜백 함수
 {
 	switch (key) {
 	case GLUT_KEY_UP: // 위쪽 화살표
-		additionalCameraPos.y += 5.0f;
 		break;
 	case GLUT_KEY_DOWN: // 아래쪽 화살표
-		additionalCameraPos.y -= 5.0f;
 		break;
 	case GLUT_KEY_LEFT: // 왼쪽 화살표
-		additionalCameraPos.x -= 5.0f;
 		break;
 	case GLUT_KEY_RIGHT: // 오른쪽 화살표
-		additionalCameraPos.x += 5.0f;
 		break;
 	}
 
 	glutPostRedisplay();
-}
-
-// 궤도와 행성/위를 그리는 함수
-void drawOrbitsAndPlanets(glm::vec3 cameraPos, glm::vec3 cameraTarget, glm::vec3 cameraUp, float rotationAngle)
-{
-	// 셰이더 사용
-	glUseProgram(shaderProgramID);
-	glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(orbitscale, orbitscale, orbitscale));
-	// 투영 행렬 설정
-	glm::mat4 projection;
-	if (projectiontoggle == 0) {
-		// 원근 투영 (Perspective)
-		projection = glm::perspective(
-			(float)(pi / 3.0f),
-			1.0f,
-			0.1f,
-			300.0f
-		);
-	}
-	else {
-		// 직교 투영 (Orthographic)
-		projection = glm::ortho(
-			-100.0f, 100.0f,  // left, right
-			-100.0f, 100.0f,  // bottom, top
-			0.1f, 300.0f      // near, far
-		);
-	}
-	unsigned int projectionLocation = glGetUniformLocation(shaderProgramID, "projectionTransform");
-	glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
-
-	// 뷰 행렬 설정
-	glm::mat4 view = glm::lookAt(cameraPos, cameraTarget, cameraUp);
-	unsigned int viewLocation = glGetUniformLocation(shaderProgramID, "viewTransform");
-	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &view[0][0]);
-
-	unsigned int modelLocation = glGetUniformLocation(shaderProgramID, "modelTransform");
-
-	// 버퍼 바인딩
-	if (!allVertices.empty()) {
-		glBindVertexArray(VAO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, allVertices.size() * sizeof(float),
-			allVertices.data(), GL_DYNAMIC_DRAW);
-	}
-
-	glLineWidth(2.0f);
-
-	// z축 회전 행렬
-	glm::mat4 zRotation = glm::rotate(glm::mat4(1.0f), rotationAngle, glm::vec3(0.0f, 0.0f, 1.0f));
-
-	// 원래 궤도 그리기 (스케일도 적용)
-	glm::mat4 orbitModel = zRotation * scaleMatrix;
-	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(orbitModel));
-	glDrawArrays(GL_LINE_LOOP, 0, 101);
-
-	// 새로운 궤도 그리기 - planetpos로 이동 -> 1/3 스케일 -> z축 회전 순서
-	glm::mat4 smallOrbitMatrix = glm::mat4(1.0f);
-
-	glm::vec3 tplanetpos = orbitscale * planetpos;
-	glm::vec3 tmoonpos = orbitscale * moonpos;
-
-	smallOrbitMatrix = glm::translate(smallOrbitMatrix, tplanetpos); // 1. 이동
-	smallOrbitMatrix = glm::scale(smallOrbitMatrix, glm::vec3(1.0f / 3.0f, 1.0f / 3.0f, 1.0f / 3.0f)); // 2. 스케일
-	smallOrbitMatrix = glm::scale(smallOrbitMatrix, glm::vec3(orbitscale, orbitscale, orbitscale)); // 2. 스케일
-	smallOrbitMatrix = zRotation * smallOrbitMatrix; // 3. z축 회전 (마지막 적용)
-	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(smallOrbitMatrix));
-	glDrawArrays(GL_LINE_LOOP, 0, 101);
-
-	// GLU 객체 그리기 (고정 파이프라인)
-	glUseProgram(0);
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-
-	if (projectiontoggle == 0) {
-		// 원근 투영
-		gluPerspective(60.0, 1.0, 0.1, 300.0);
-	}
-	else {
-		// 직교 투영
-		glOrtho(-100.0, 100.0, -100.0, 100.0, 0.1, 300.0);
-	}
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(cameraPos.x, cameraPos.y, cameraPos.z,
-		cameraTarget.x, cameraTarget.y, cameraTarget.z,
-		cameraUp.x, cameraUp.y, cameraUp.z);
-
-	// z축 회전을 GLU 객체에도 적용
-	glRotatef(glm::degrees(rotationAngle), 0.0f, 0.0f, 1.0f);
-
-	// 초록색 행성 그리기
-	glPushMatrix();
-	glTranslatef(tplanetpos.x, tplanetpos.y, tplanetpos.z);
-
-	GLUquadricObj* qobj2 = gluNewQuadric();
-	if (wiretoggle == 1) {
-		gluQuadricDrawStyle(qobj2, GLU_LINE); // 와이어프레임
-	}
-	else {
-		gluQuadricDrawStyle(qobj2, GLU_FILL); // 솔리드
-	}
-	glColor3f(0.0f, 1.0f, 0.0f);
-	gluSphere(qobj2, 5.0, spherelevel, spherelevel);
-	gluDeleteQuadric(qobj2);
-
-	glPopMatrix();
-
-	// 빨간색 위성 그리기
-	glPushMatrix();
-	glTranslatef(tplanetpos.x, tplanetpos.y, tplanetpos.z);
-	glTranslatef(tmoonpos.x, tmoonpos.y, tmoonpos.z);
-
-	GLUquadricObj* qobj3 = gluNewQuadric();
-	if (wiretoggle == 1) {
-		gluQuadricDrawStyle(qobj3, GLU_LINE); // 와이어프레임
-	}
-	else {
-		gluQuadricDrawStyle(qobj3, GLU_FILL); // 솔리드
-	}
-	glColor3f(1.0f, 0.0f, 0.0f);
-	gluSphere(qobj3, 2.5, spherelevel, spherelevel);
-	gluDeleteQuadric(qobj3);
-
-	glPopMatrix();
 }
